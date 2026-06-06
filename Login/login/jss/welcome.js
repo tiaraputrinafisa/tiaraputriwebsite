b335c45891f06471f884f6d5b086dd5bb98c459c
doopqt <dominikusapaja@gmail.com>
2026-06-06 14:33:10 +0000

    Update welcome.js

--- Login/login/jss/welcome.js
+++ Login/login/jss/welcome.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     function goLogin() {
-        window.location.href = "login/index.html";
+        window.location.href = "Login/login/index.html";
     }
 
     function logout() {
